817e2c1e90a0a6faa93db44cd5e879409921de04
a-onkar <mailanandteerth@gmail.com>
2020-06-03 15:25:54 +0100

    Updating repo

--- cypress/integration/Test Automation University/delay-timeout.js
+++
@@ -1,8 +0,0 @@
-
-
-it('Adding delay and time-out', ()=>{
-    cy.visit('http://todomvc-app-for-testing.surge.sh/?delay-new-todo=5000') // delays the elemtn with classname to appear
-    
-    cy.get('.new-todo',{timeout:6000}).type('Learn JavaScript {enter}')
-
-})
